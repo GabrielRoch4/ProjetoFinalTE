3a5d2bb08695e336d243d187367390e0c2778d17
arthurfcarvalho <satierfruhtra@protonmail.com>
2024-08-25 11:49:42 -0300

    tentativa de fix no atribuirNota

--- repositories/notaRepository.go
+++ repositories/notaRepository.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"ProjetoFinal/configs/database"
 	"ProjetoFinal/models"
+	"gorm.io/gorm"
 )
 
 type NotaRepository interface {
@@ -33,7 +34,9 @@ func (r *NotaRepositoryImpl) FindByID(id uint) (*models.Nota, error) {
 }
 
 func (r *NotaRepositoryImpl) Update(nota *models.Nota) error {
-	return database.DB.Model(nota).Updates(nota).Error
+	return database.DB.Model(nota).Updates(map[string]interface{}{
+        "Nota": nota.Nota,
+    }).Error
 }
 
 func (r *NotaRepositoryImpl) Delete(id uint) error {
@@ -41,10 +44,13 @@ func (r *NotaRepositoryImpl) Delete(id uint) error {
 }
 
 func (r *NotaRepositoryImpl) FindByAlunoAndAtividade(alunoID, atividadeID uint) (*models.Nota, error) {
-	var nota models.Nota
-	err := database.DB.Where("aluno_id = ? AND atividade_id = ?", alunoID, atividadeID).First(&nota).Error
-	if err != nil {
-		return nil, err
-	}
-	return &nota, nil
+    var nota models.Nota
+    err := database.DB.Where("aluno_id = ? AND atividade_id = ?", alunoID, atividadeID).First(&nota).Error
+    if err != nil {
+        if err == gorm.ErrRecordNotFound {
+            return nil, nil
+        }
+        return nil, err
+    }
+    return &nota, nil
 }
